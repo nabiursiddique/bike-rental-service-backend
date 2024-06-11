9f5c93628f052d6b9a901b440d85bf7fa0b94753
nabiur siddique <nabiursiddique01@gmail.com>
2024-06-12 02:54:19 +0600

    booking model is created

---
+++ src/modules/booking/booking.model.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose';
+import { TBooking } from './booking.interface';
+
+const bookingSchema = new Schema<TBooking>({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'User Id is required'],
+    unique: true,
+    ref: 'User',
+  },
+  bikeId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    unique: true,
+    ref: 'Bike',
+  },
+  startTime: {
+    type: Date,
+    required: true,
+  },
+  returnTime: {
+    type: Date,
+    required: true,
+  },
+  totalCost: {
+    type: Number,
+    required: true,
+  },
+  isReturned: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+export const Booking = model<TBooking>('Booking', bookingSchema);
