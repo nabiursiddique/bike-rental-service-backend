0212a4ea48f8f672ea0fe50fd89f531011c80c38
nabiur siddique <nabiursiddique01@gmail.com>
2024-06-12 02:06:41 +0600

    user and bike interface is created

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,9 @@ export const app = express();
 app.use(express.json());
 app.use(cors());
 
+// not found route
+app.use(notFoundRoute);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Bike rental app is running......');
 });
-
-app.use(notFoundRoute);
