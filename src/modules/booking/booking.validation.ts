03e30eaece7080144962c3b5629ae8e16f9968dc
nabiur siddique <nabiursiddique01@gmail.com>
2024-06-12 09:39:47 +0600

    booking validation is created

---
+++ src/modules/booking/booking.validation.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+const bookingSchema = z.object({
+  userId: z.string().min(1, { message: 'User Id is required' }),
+  bikeId: z.string().min(1, { message: 'Bike Id is required' }),
+  startTime: z.date({ required_error: 'Start time is required' }),
+  returnTime: z.date({ required_error: 'Return time is required' }),
+  totalCost: z.number({ required_error: 'Total cost is required' }),
+  isReturned: z.boolean({ required_error: 'Is Returned is required' }),
+});
+
+export default bookingSchema;
